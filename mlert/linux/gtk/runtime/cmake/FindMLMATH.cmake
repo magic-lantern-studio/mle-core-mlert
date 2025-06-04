2e4ca6077a30128baaa754048b26bd4d6f68b177
Mark Millard <msm@wizzerworks.com>
2025-06-04 16:15:35 -0600

    Checkpoint for CMake build environment implementation.

--- mlert/linux/gtk/runtime/cmake/FindMLMATH.cmake
+++ mlert/linux/gtk/runtime/cmake/FindMLMATH.cmake
@@ -1,35 +1,73 @@
+# COPYRIGHT_BEGIN
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2025 Wizzer Works
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#  For information concerning this header file, contact Mark S. Millard,
+#  of Wizzer Works at msm@wizzerworks.com.
+#
+#  More information concerning Wizzer Works may be found at
+#
+#      http://www.wizzerworks.com
+#
+# COPYRIGHT_END
+
 include(FindPackageHandleStandardArgs)
 include(SelectLibraryConfigurations)
 
 if (DEFINED ENV{MLE_ROOT})
-  message(STATUS "MLE_ROOT=" $ENV{MLE_ROOT})
+  message(STATUS "  FindMLMATH MLE_ROOT=" $ENV{MLE_ROOT})
 else()
-  message(STATUS "MLE_ROOT NOT SET")
+  message(STATUS "  FindMLMATH MLE_ROOT NOT SET")
   set(ENV{MLE_ROOT} "/opt/MagicLantern")
-  message(STATUS "Using " $ENV{MLE_ROOT})
+  message(STATUS "  FindMLMATH Using " $ENV{MLE_ROOT})
 endif()
 
 find_path(
   MLMATH_INCLUDE_DIR
-  NAMES mle/mlmath.h
-  PATHS $ENV{MLE_ROOT}/include)
+  NAMES mlmath.h
+  PATHS $ENV{MLE_ROOT}/include/mle
+  NO_CACHE)
 
 find_library(
   MLMATH_LIBRARY_DEBUG
-  NAMES mlertd
+  NAMES mlmathd
   PATHS $ENV{MLE_ROOT}/lib
-  )
+  PATH_SUFFIXES mle)
 
 find_library(
   MLMATH_LIBRARY_RELEASE
-  NAMES mlert
+  NAMES mlmath
   PATHS $ENV{MLE_ROOT}/lib
-  )
+  PATH_SUFFIXES mle)
+
+#message(STATUS "  FindMLMATH MLMATH_INCLUDE_DIR=" ${MLMATH_INCLUDE_DIR})
+#message(STATUS "  FindMLMATH MLMATH_LIBRARY_DEBUG=" ${MLMATH_LIBRARY_DEBUG})
+#message(STATUS "  FindMLMATH MLMATH_LIBRARY_RELEASE=" ${MLMATH_LIBRARY_RELEASE})
 
 select_library_configurations(MLMATH)
 
-if(MLMATH_INCLUDE_DIR AND EXISTS "${MLMATH_INCLUDE_DIR}/mle/mlmath.h")
-  file(STRINGS "${MLMATH_INCLUDE_DIR}/mle/mlmath.h" _MLMATH_VERSION_DEFINE REGEX "[\t ]*#define[\t ]+MLMATH_VERSION[\t ]+\"[^\"]*\".*")
+if(MLMATH_INCLUDE_DIR AND EXISTS "${MLMATH_INCLUDE_DIR}/mlmath.h")
+  file(STRINGS "${MLMATH_INCLUDE_DIR}/mlmath.h" _MLMATH_VERSION_DEFINE REGEX "[\t ]*#define[\t ]+MLMATH_VERSION[\t ]+\"[^\"]*\".*")
   string(REGEX REPLACE "[\t ]*#define[\t ]+MLMATH_VERSION[\t ]+\"([^\"]*)\".*" "\\1" MLMATH_VERSION "${_MLMATH_VERSION_DEFINE}")
   unset(_MLMATH_VERSION_DEFINE)
 endif()
@@ -42,8 +80,7 @@ find_package_handle_standard_args(
   MLMATH
   FOUND_VAR MLMATH_FOUND
   REQUIRED_VARS MLMATH_INCLUDE_DIR MLMATH_LIBRARY
-  VERSION_VAR MLMATH_VERSION
-  )
+  VERSION_VAR MLMATH_VERSION)
 
 if(MLMATH_FOUND AND NOT TARGET MLMath::MLMath)
   add_library(MlMath::MlMath UNKNOWN IMPORTED)
@@ -51,11 +88,11 @@ if(MLMATH_FOUND AND NOT TARGET MLMath::MLMath)
     set_property(TARGET MlMath::MlMath APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
     set_target_properties(MlMath::MlMath PROPERTIES IMPORTED_LOCATION_RELEASE "${MLMATH_LIBRARY_RELEASE}")
   endif()
-  if(MlMath_LIBRARY_DEBUG)
+  if(MLMATH_LIBRARY_DEBUG)
     set_property(TARGET MlMath::MlMath APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
     set_target_properties(MlMath::MlMath PROPERTIES IMPORTED_LOCATION_DEBUG "${MLMATH_LIBRARY_DEBUG}")
   endif()
-  set_target_properties(MlMath::MlMath PROPERTIES INTERFACE_COMPILE_DEFINITIONS "COIN_DLL")
+  set_target_properties(MlMath::MlMath PROPERTIES INTERFACE_COMPILE_DEFINITIONS "")
   set_target_properties(MlMath::MlMath PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MLMATH_INCLUDE_DIRS}")
 endif()
 
--- mlert/linux/gtk/runtime/cmake/FindMLUTIL.cmake
+++ mlert/linux/gtk/runtime/cmake/FindMLUTIL.cmake
@@ -1,37 +1,73 @@
+# COPYRIGHT_BEGIN
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2025 Wizzer Works
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#  For information concerning this header file, contact Mark S. Millard,
+#  of Wizzer Works at msm@wizzerworks.com.
+#
+#  More information concerning Wizzer Works may be found at
+#
+#      http://www.wizzerworks.com
+#
+# COPYRIGHT_END
+
 include(FindPackageHandleStandardArgs)
 include(SelectLibraryConfigurations)
 
 if (DEFINED ENV{MLE_ROOT})
-  message(STATUS "MLE_ROOT=" $ENV{MLE_ROOT})
+  message(STATUS "  FindMLUTIL MLE_ROOT=" $ENV{MLE_ROOT})
 else()
-  message(STATUS "MLE_ROOT NOT SET")
+  message(STATUS "  FindMLUTIL MLE_ROOT NOT SET")
   set(ENV{MLE_ROOT} "/opt/MagicLantern")
-  message(STATUS "Using " $ENV{MLE_ROOT})
+  message(STATUS "  FindMLUTIL Using " $ENV{MLE_ROOT})
 endif()
 
 find_path(
   MLUTIL_INCLUDE_DIR
-  NAMES mle/mle.h
-  PATHS $ENV{MLE_ROOT}/include)
+  NAMES mle.h
+  PATHS $ENV{MLE_ROOT}/include/mle
+  NO_CACHE)
 
 find_library(
   MLUTIL_LIBRARY_DEBUG
   NAMES mlutild
   PATHS $ENV{MLE_ROOT}/lib
-  PATH_SUFFIXES mle
-  )
+  PATH_SUFFIXES mle)
 
 find_library(
   MLUTIL_LIBRARY_RELEASE
   NAMES mlutil
   PATHS $ENV{MLE_ROOT}/lib
-  PATH_SUFFIXES mle
-  )
+  PATH_SUFFIXES mle)
 
 select_library_configurations(MLUTIL)
 
-if(MLMath_INCLUDE_DIR AND EXISTS "${MLUTIL_INCLUDE_DIR}/mle/mlutil.h")
-  file(STRINGS "${MLUTIL_INCLUDE_DIR}/mle/mlutil.h" _MLUTIL_VERSION_DEFINE REGEX "[\t ]*#define[\t ]+MLUTIL_VERSION[\t ]+\"[^\"]*\".*")
+#message(STATUS "  FindMLUTIL MLUTIL_INCLUDE_DIR=" ${MLUTIL_INCLUDE_DIR})
+#message(STATUS "  FindMLUTIL MLUTIL_LIBRARY_DEBUG=" ${MLUTIL_LIBRARY_DEBUG})
+#message(STATUS "  FindMLUTIL MLUTIL_LIBRARY_RELEASE=" ${MLUTIL_LIBRARY_RELEASE})
+
+if(MLMath_INCLUDE_DIR AND EXISTS "${MLUTIL_INCLUDE_DIR}/mlutil.h")
+  file(STRINGS "${MLUTIL_INCLUDE_DIR}/mlutil.h" _MLUTIL_VERSION_DEFINE REGEX "[\t ]*#define[\t ]+MLUTIL_VERSION[\t ]+\"[^\"]*\".*")
   string(REGEX REPLACE "[\t ]*#define[\t ]+MLUTIL_VERSION[\t ]+\"([^\"]*)\".*" "\\1" MLUTIL_VERSION "${_MLUTIL_VERSION_DEFINE}")
   unset(_MLUTIL_VERSION_DEFINE)
 endif()
@@ -44,8 +80,7 @@ find_package_handle_standard_args(
   MLUTIL
   FOUND_VAR MLUTIL_FOUND
   REQUIRED_VARS MLUTIL_INCLUDE_DIR MLUTIL_LIBRARY
-  VERSION_VAR MLUTIL_VERSION
-  )
+  VERSION_VAR MLUTIL_VERSION)
 
 if(MLUTIL_FOUND AND NOT TARGET MlUtil::MlUtil)
   add_library(MlUtil::MlUtil UNKNOWN IMPORTED)
@@ -53,7 +88,7 @@ if(MLUTIL_FOUND AND NOT TARGET MlUtil::MlUtil)
     set_property(TARGET MlUtil::MlUtil APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
     set_target_properties(MlUtil::MlUtil PROPERTIES IMPORTED_LOCATION_RELEASE "${MLUTIL_LIBRARY_RELEASE}")
   endif()
-  if(MlUtil_LIBRARY_DEBUG)
+  if(MLUTIL_LIBRARY_DEBUG)
     set_property(TARGET MlUtil::MlUtil APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
     set_target_properties(MlUtil::MlUtil PROPERTIES IMPORTED_LOCATION_DEBUG "${MLUTIL_LIBRARY_DEBUG}")
   endif()
